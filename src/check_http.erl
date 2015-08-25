67e3e02d484e7c3fdcd204c7291670a46fa22be7
Varoun. P <contact@varoun.com>
2015-08-25 09:23:30 +0000

    Add support for periodic checks at the lowest level, i.e, with check_http.

--- src/check_http.erl
+++ src/check_http.erl
@@ -14,7 +14,8 @@
 
 start_link([{real, HostName},
             {path, _Path},
-            {timeout, _Timeout}] = Options) ->
+            {timeout, _Timeout},
+            {frequency, _Frequency}] = Options) ->
     gen_server:start_link({local, list_to_atom(HostName)}, ?MODULE, Options, []).
 
 check(Hostname) ->
@@ -22,29 +23,60 @@ check(Hostname) ->
 
 %%%% gen_server callbacks ------------------------------------------------------
 
-init([{real, HostName}, {path, Path}, {timeout, Timeout}] = _Options) ->
-    {ok, [{url, "http://" ++ HostName ++ Path}, {timeout, Timeout}]}.
+init([{real, HostName},
+      {path, Path},
+      {timeout, Timeout},
+      {frequency, Frequency}] = _Options) ->
+    timer:send_after(Frequency, self(), trigger),
+    {ok, [{url, "http://" ++ HostName ++ Path}, {timeout, Timeout},
+          {frequency, Frequency}, {status, init}]}.
 
 
-handle_call(check_health, _From, [{url, URL}, {timeout, Timeout}] = Options) ->
+handle_call(check_health, _From,
+            [{url, _URL}, {timeout, _Timeout},
+             {frequency, _Frequency}, {status, Status}] = State) ->
+    {reply, Status, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(trigger, [{url, URL}, {timeout, Timeout},
+                      {frequency, Frequency}, {status, _Status}] = State) ->
     try httpc:request(get, {URL, []}, [{timeout, Timeout}], []) of
         {ok, {{_Version, 200, _ReasonPhrase}, _Headers, _Body}} ->
-            {reply, healthy, Options};
+            timer:send_after(Frequency, self(), trigger),
+            {noreply, [{url, URL}, {timeout, Timeout},
+                       {frequency, Frequency}, {status, healthy}]};
         {error, Reason} ->
-            {reply, {unhealthy, Reason}, Options}
+            timer:send_after(Frequency, self(), trigger),
+            {noreply, [{url, URL}, {timeout, Timeout},
+                       {frequency, Frequency}, {status, {unhealthy, Reason}}]}
     catch
         _:Reason ->
-            {stop, Reason, Options}
-    end.
+            {stop, Reason, State}
+    end;
+handle_info(_Msg, State) ->
+    {noreply, State}.
 
-handle_cast(_Request, Options) ->
-    {noreply, Options}.
-
-handle_info(_Msg, Options) ->
-    {noreply, Options}.
-
-terminate(_Reason, _Options) ->
+terminate(_Reason, _State) ->
     ok.
 
-code_change(_OldVsn, Options, _Extra) ->
-    {ok, Options}.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%% 1> c("/home/varoun/dev/rotor/src/check_http", [{outdir, "/home/varoun/dev/rotor/src/"}]).
+%% c("/home/varoun/dev/rotor/src/check_http", [{outdir, "/home/varoun/dev/rotor/src/"}]).
+%% {ok,check_http}
+%% 2> check_http:start_link([{real, "www.freebsd.org"}, {path, "/"}, {timeout, 2000},
+%% 2>                        {frequency, 30000}]).
+%% {ok,<0.40.0>}
+%% 3> inets:start().
+%% ok
+%% 4> check_http:check("www.freebsd.org").
+%% init
+%% 5> check_http:check("www.freebsd.org").
+%% healthy
+%% 6> check_http:check("www.freebsd.org").
+%% healthy
+%% 7>
