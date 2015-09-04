1f404c9b7b68dd0ba48eb0b2860ba9c26d761ffa
Varoun. P <contact@varoun.com>
2015-09-04 09:40:49 +0000

    Remove sync calls from check_http.

--- src/check_http.erl
+++ src/check_http.erl
@@ -29,31 +29,42 @@ init([{real, HostName},
       {frequency, Frequency}] = _Options) ->
     timer:send_after(Frequency, self(), trigger),
     {ok, [{url, "http://" ++ HostName ++ Path}, {timeout, Timeout},
-          {frequency, Frequency}, {status, init}]}.
+          {frequency, Frequency}, {requestid, none}, {status, init}]}.
 
 
 handle_call(check_health, _From,
-            [_URL, _Timeout, _Frequency, {status, Status}] = State) ->
+            [_URL, _Timeout, _Frequency, _RequestID,
+             {status, Status}] = State) ->
     {reply, Status, State}.
 
 handle_cast(_Request, State) ->
     {noreply, State}.
 
-handle_info(trigger, [{url, URL}, {timeout, Timeout},
-                      {frequency, Frequency}, {status, _Status}] = State) ->
-    try httpc:request(get, {URL, []}, [{timeout, Timeout}], []) of
-        {ok, {{_Version, 200, _ReasonPhrase}, _Headers, _Body}} ->
-            timer:send_after(Frequency, self(), trigger),
-            {noreply, [{url, URL}, {timeout, Timeout},
-                       {frequency, Frequency}, {status, healthy}]};
+handle_info(trigger, [{url, URL},
+                      {timeout, Timeout},
+                      {frequency, Frequency},
+                      _RequestID,
+                      Status] = _State) ->
+    {ok, RequestID} =
+        httpc:request(get, {URL, []}, [{timeout, Timeout}],
+                      [{sync, false}, {receiver, self()}]),
+    timer:send_after(Frequency, self(), trigger),
+    {noreply, [{url, URL}, {timeout, Timeout},
+               {frequency, Frequency}, {requestid, RequestID},
+               Status]};
+handle_info({http, {RequestID, Result}},
+            [URL, Timeout, Frequency,
+             {requestid, RequestID},
+             _Status] = _State) ->
+    case Result of
+        {{_Version, 200, _ReasonPhrase}, _Headers, _Body} ->
+            {noreply, [URL, Timeout, Frequency,
+                       {requestid, none}, {status, healthy}]};
         {error, Reason} ->
-            timer:send_after(Frequency, self(), trigger),
-            {noreply, [{url, URL}, {timeout, Timeout},
-                       {frequency, Frequency}, {status, {unhealthy, Reason}}]}
-    catch
-        _:Reason ->
-            {stop, Reason, State}
+            {noreply, [URL, Timeout, Frequency,
+                       {requestid, none}, {status, {unhealthy, Reason}}]}
     end;
+
 handle_info(_Msg, State) ->
     {noreply, State}.
 
@@ -63,19 +74,41 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+%% varoun@ip-10-0-2-24:~/dev/rotor % rebar3 compile
+%% ===> Verifying dependencies...
+%% ===> Compiling rotor
+%% varoun@ip-10-0-2-24:~/dev/rotor % rebar3 shell
+%% ===> Verifying dependencies...
+%% ===> Compiling rotor
+%% Erlang/OTP 18 [erts-7.0.2] [source] [64-bit] [async-threads:0] [hipe] [kernel-poll:false] [dtrace]
 
-%% 1> c("/home/varoun/dev/rotor/src/check_http", [{outdir, "/home/varoun/dev/rotor/src/"}]).
-%% c("/home/varoun/dev/rotor/src/check_http", [{outdir, "/home/varoun/dev/rotor/src/"}]).
-%% {ok,check_http}
-%% 2> check_http:start_link([{real, "www.freebsd.org"}, {path, "/"}, {timeout, 2000},
-%% 2>                        {frequency, 30000}]).
-%% {ok,<0.40.0>}
-%% 3> inets:start().
-%% ok
-%% 4> check_http:check("www.freebsd.org").
+%% Eshell V7.0.2  (abort with ^G)
+%% 1> check_http:start_link([{real, "www.freebsd.org"}, {path, "/"}, {timeout, 2000}, {frequency, 30000}]).
+%% {ok,<0.78.0>}
+%% 2> check_http:check("www.freebsd.org").
 %% init
+%% 3> check_http:check("www.freebsd.org").
+%% init
+%% 4> check_http:check("www.freebsd.org").
+%% healthy
 %% 5> check_http:check("www.freebsd.org").
 %% healthy
-%% 6> check_http:check("www.freebsd.org").
-%% healthy
-%% 7>
+%% 6> q().
+%% ok
+%% 7> varoun@ip-10-0-2-24:~/dev/rotor % rebar3 shell
+%% ===> Verifying dependencies...
+%% ===> Compiling rotor
+%% Erlang/OTP 18 [erts-7.0.2] [source] [64-bit] [async-threads:0] [hipe] [kernel-poll:false] [dtrace]
+
+%% Eshell V7.0.2  (abort with ^G)
+%% 1> check_http:start_link([{real, "www.gfreebsd.org"}, {path, "/"}, {timeout, 2000}, {frequency, 30000}]).
+%% {ok,<0.78.0>}
+%% 2>  check_http:check("www.gfreebsd.org").
+%% init
+%% 3>  check_http:check("www.gfreebsd.org").
+%% {unhealthy,{failed_connect,[{to_address,{"www.gfreebsd.org",
+%%                                          80}},
+%%                             {inet,[inet],nxdomain}]}}
+%% 4> q().
+%% ok
+%% 5> varoun@ip-10-0-2-24:~/dev/rotor %
