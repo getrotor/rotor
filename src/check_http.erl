abc53dea67a9a9d0b4797bd46e236c349e242fde
Varoun. P <contact@varoun.com>
2015-10-12 01:06:41 -0400

    Start adding support for tcp checks.

--- src/check_http.erl
+++ src/check_http.erl
@@ -28,8 +28,10 @@ init(#realconf{ip=IP, ping_protocol=http, ping_port=Port,
     timer:send_after(?WAITTIME, self(), trigger),
     {ok, [#checkstate{options = Options},
           {url, "http://" ++ IP ++ ":" ++ integer_to_list(Port) ++ Path},
-          {requestid, none}]}.
-
+          {requestid, none}]};
+init(#realconf{} = Options) ->
+    timer:send_after(?WAITTIME, self(), trigger),
+    {ok, [#checkstate{options = Options}, tcp_check]}.
 
 handle_call(check_health, _From,
             [#checkstate{status = Status}, _URL, _ReqID] = State) ->
@@ -129,6 +131,56 @@ handle_info({http, {RequestID, Result}},
                                              status = unhealthy},
                        URL,
                        {requestid, none}]}
+    end;
+
+handle_info(trigger, [#checkstate{options=Options,
+                                   healthy_count=HealthyCount,
+                                   unhealthy_count=UnhealthyCount,
+                                   status=Status} = CheckState,
+                      tcp_check]) ->
+    IP = Options#realconf.ip,
+    PORT = Options#realconf.ping_port,
+    case gen_tcp:connect(IP, PORT, [], Options#realconf.response_timeout) of
+        {ok, Socket} when Status =:= healthy ->
+            gen_tcp:close(Socket),
+            {noreply, [CheckState#checkstate{healthy_count=0,
+                                             unhealthy_count=0},
+                       tcp_check]};
+        {ok, Socket} when Status =:= unhealthy andalso
+                          HealthyCount < Options#realconf.healthy_threshold ->
+            gen_tcp:close(Socket),
+            lager:notice("Service ~p transitioning to status HEALTHY",
+                         [IP ++ ":" ++ PORT]),
+            {noreply, [CheckState#checkstate{healthy_count = HealthyCount + 1,
+                                             unhealthy_count = 0},
+                       tcp_check]};
+        {ok, Socket} when Status =:= unhealthy andalso
+                          HealthyCount =:= Options#realconf.healthy_threshold ->
+            gen_tcp:close(Socket),
+            lager:notice("Service ~p has transitioned to status HEALTHY",
+                         [IP ++ ":" ++ PORT]),
+            gen_server:cast(list_to_atom(Options#realconf.name), trigger),
+            {noreply, [CheckState#checkstate{unhealthy_count = 0, status = healthy},
+                       tcp_check]};
+        {error, _Reason} when Status =:= unhealthy ->
+            {noreply, [CheckState#checkstate{healthy_count = 0,
+                                             unhealthy_count = 0},
+                       tcp_check]};
+        {error, _Reason} when Status =:= healthy andalso
+                              UnhealthyCount < Options#realconf.unhealthy_threshold ->
+            lager:notice("Service ~p transitioning to status UNHEALTHY",
+                         [IP ++ ":" ++ PORT]),
+            {noreply, [CheckState#checkstate{healthy_count = 0,
+                                             unhealthy_count = UnhealthyCount + 1},
+                       tcp_check]};
+        {error, _Reason} when Status =:= healthy andalso
+                              UnhealthyCount =:= Options#realconf.unhealthy_threshold ->
+            lager:notice("Service ~p has transitioned to status UNHEALTHY",
+                         [IP ++ ":" ++ PORT]),
+            gen_server:cast(list_to_atom(Options#realconf.name), trigger),
+            {noreply, [CheckState#checkstate{healthy_count = 0,
+                                             status = unhealthy},
+                       tcp_check]}
     end;
 
 %% NOTE(varoun): Delete the next function?
